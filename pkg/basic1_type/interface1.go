f18c116e9cc84523287e391f2ea884c7a8806fd1
赵 卫国 <programfan.info@gmail.com>
2019-12-23 14:00:46 +0800

    update

---
+++ pkg/basic1_type/interface1.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+
+	// int转为interface{}默认直接转
+	var i int
+	i = 1
+	log.Println(i)
+	doInterface(i)
+	log.Println("------------")
+
+	// map[string]int转为map[string]interface{}需要专门手工转
+	var maps map[string]int
+	maps = make(map[string]int)
+	maps["a"] = 1
+	maps["b"] = 2
+	log.Println(maps)
+	var maps2 map[string]interface{}
+	maps2 = make(map[string]interface{})
+	for key, value := range maps {
+		maps2[key] = value
+		log.Println("+")
+	}
+	doInterfaces(maps2)
+	log.Println(maps2)
+}
+
+func doInterface(variable interface{}) {
+	log.Println("++", variable)
+}
+
+func doInterfaces(variables map[string]interface{}) {
+	for key, variable := range variables {
+		log.Println("+++", key, variable)
+	}
+}
--- pkg/time/demo2_Duration.go
+++ pkg/time/demo2_Duration.go
@@ -1,24 +1,26 @@
 package main
 
-import(
+import (
 	"fmt"
 	"time"
 )
 
 func main() {
-	timeout := time.Duration(1) * time.Second
-	fmt.Printf("timeout: %d\n", timeout)   // 1000,000,000
+
+	timeout := time.Duration(1) * time.Microsecond
+	time.Sleep(timeout)
+	fmt.Printf("timeout: %d\n", timeout) // 1000
 
 	timeout = time.Duration(1) * time.Millisecond
-	fmt.Printf("timeout: %d\n", timeout)   // 1000,000
+	time.Sleep(timeout)
+	fmt.Printf("timeout: %d\n", timeout) // 1000,000
 
-	timeout = time.Duration(1) * time.Microsecond
-	fmt.Printf("timeout: %d\n", timeout)   // 1000
+	timeout = time.Duration(1) * time.Second
+	time.Sleep(timeout)
+	fmt.Printf("timeout: %d\n", timeout) // 1000,000,000
 
 	timeout = time.Duration(1) * time.Minute
-	fmt.Printf("timeout: %d\n", timeout)   // 60,000,000,000
-
-
-
+	time.Sleep(timeout)
+	fmt.Printf("timeout: %d\n", timeout) // 60,000,000,000
 
 }
