7e34181d9e6bfb76e71bb8a79387326212d54068
赵 卫国 <programfan.info@gmail.com>
2020-08-11 14:36:05 +0800

    update

--- pkg/reflect/simple/lib.go
+++ pkg/reflect/simple/lib.go
@@ -1,23 +1,6 @@
-package main
+package simple
 
-import (
-	"fmt"
-	"reflect"
-	"sort"
-)
-
-/*
-排序并去除重复字符
-*/
-func main() {
-	b := []string{"a", "b", "c", "c", "e", "f", "a", "g", "b", "b", "c"}
-	sort.Strings(b)
-	fmt.Println(Duplicate(b))
-
-	c := []int{1, 1, 2, 4, 6, 7, 8, 4, 3, 2, 5, 6, 6, 8}
-	sort.Ints(c)
-	fmt.Println(Duplicate(c))
-}
+import "reflect"
 
 // 解决所有类型的去除重复数据问题
 func Duplicate(a interface{}) (ret []interface{}) {
